403b36777d1726eee623c00b0f290a01d736463e
Dan Greco <git@dangre.co>
2026-04-25 00:22:25 -0400

    Fix/oxc arguments (#33)
    * fix(node): proper oxc lsp arguments
    
    * fix(node): enable vtsls

--- template/node/flake.nix
+++ template/node/flake.nix
@@ -44,9 +44,10 @@
                       language_servers = [
                         "oxfmt"
                         "oxlint"
+                        "vtsls"
                         "!typescript-language-server"
-                        "!vtsls"
                         "!eslint"
+                        "!biome"
                       ];
                       formatter = [
                         {
@@ -66,8 +67,14 @@
                     };
                   in
                   pkgs.writers.writeJSON "settings.json" {
-                    lsp.oxlint.binary.path = "${pkgs.oxlint}/bin/oxlint";
-                    lsp.oxfmt.binary.path = "${pkgs.oxfmt}/bin/oxfmt";
+                    lsp.oxlint.binary = {
+                      path = "${pkgs.oxlint}/bin/oxlint";
+                      arguments = [ "--lsp" ];
+                    };
+                    lsp.oxfmt.binary = {
+                      path = "${pkgs.oxfmt}/bin/oxfmt";
+                      arguments = [ "--lsp" ];
+                    };
 
                     languages.JavaScript = defaults;
                     languages.JSX = defaults;
